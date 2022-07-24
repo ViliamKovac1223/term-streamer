0be6960fddf99440368e19c07cc8a44dcc58cc63
ViliamKovac1223 <viliamkovac1223@gmail.com>
2022-07-24 02:25:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Term_streamer
+Do you want to stream your videos from your computer to your phone without leaving your comfy terminal ?
+Yes, so this project is just for you. Term_streamer allows you to stream videos from your terminal to your phone or another
+computer or just about anything that supports modern web browser such as firefox or chromium.
+Term_streamer create flask server which can stream video to multiple devices at the same time.
+
+# Usage
+Basic usage of term_streamer
+
+```bash
+./term_streamer.py -f /path/to/the/video.mp4
+```
+
+# All command options
+-h # print help menu
+
+-f <file> # file to stream, this option is MANDATORY
+
+-p <port> # port to stream on, default port is 8000
+
+# Installation
+
+## Install dependencies
+```bash
+sudo pip install -r requirements.txt
+```
+
+## Install program
+Simply add this folder to your $PATH variable.
---
+++ term_streamer.py
@@ -0,0 +1,68 @@
+#!/bin/python
+
+import os
+import sys
+import getopt
+import socket
+from flask import Flask, send_file, make_response
+from dataclasses import dataclass
+
+@dataclass
+class Options:
+    file:str
+    port:int
+    ip:str
+
+DEFAULT_PORT=8000
+APP = Flask(__name__)
+program_options = Options("", DEFAULT_PORT, "")
+
+def main(argv):
+    # parse arguments
+    try:
+        opts, args = getopt.getopt(argv,"hf:p:")
+    except getopt.GetoptError:
+        help_options()
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if (opt == "-h"):
+            help_options()
+            exit()
+        elif (opt == "-f"):
+            try:
+                program_options.file = str(arg)
+            except ValueError: # error if user didn't pass path after -f flag
+                print("-f flag should be followed by file path")
+                sys.exit(1)
+        elif (opt == "-p"):
+            try:
+                program_options.port = int(str(arg))
+            except ValueError: # error if user didn't pass port after -p flag
+                print("-p flag should be followed by port number (int)")
+                sys.exit(1)
+    
+    program_options.ip = socket.gethostbyname(socket.gethostname()) # get an ip address
+    if (program_options.file != ""):
+        server_url = f"http://{program_options.ip}:{program_options.port}"
+        print(server_url) # print server url
+
+        # run web server
+        APP.run(host=program_options.ip, port=program_options.port, debug=False, use_reloader=False)
+    else: # print help menu if user didn't specify a file
+        help_options()
+
+def help_options():
+    print("-h # print help menu")
+    print("-f <file> # file to stream, this option is MANDATORY")
+    print(f"-p <port> # port to stream on, default port is {DEFAULT_PORT}")
+
+@APP.route('/')
+def serve_video():
+    vid_path = os.path.expanduser(program_options.file)
+    resp = make_response(send_file(vid_path))
+    resp.headers['Content-Disposition'] = 'inline'
+    return resp
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
